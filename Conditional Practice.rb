96ee0570b84cde4f2d02a607bb5c7de6c237626f
totallynotleo <leodabrim@gmail.com>
2019-02-22 09:30:54 +1000

    Added older than 100

--- Conditional Practice.rb
+++ Conditional Practice.rb
@@ -3,9 +3,10 @@ name = gets.chomp
 puts "Hi #{name}! How old are you?"
 age = gets.chomp.to_i
 till100 = 100-age
-puts "Wow! You are  #{age} years old!"
-puts "#{name}, in #{till100} you will be 100 years old!"
-
-puts "Would you like to be more precise?"
-input = gets.chomp 
-
+from100 = age - 100
+if age >= 100
+    puts "You're already 100!"
+    puts "#{name}, you turned 100 #{from100} years ago"
+else 
+    puts "Wow! You are  #{age} years old!"
+    puts "#{name}, in #{till100} you will be 100 years old!"
