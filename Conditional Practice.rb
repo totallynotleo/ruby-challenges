2487612d410996e62b562cd937960181874528ca
totallynotleo <leodabrim@gmail.com>
2019-02-22 09:34:50 +1000

    testing

--- Conditional Practice.rb
+++ Conditional Practice.rb
@@ -4,9 +4,11 @@ puts "Hi #{name}! How old are you?"
 age = gets.chomp.to_i
 till100 = 100-age
 from100 = age - 100
+
 if age >= 100
     puts "You're already 100!"
     puts "#{name}, you turned 100 #{from100} years ago"
 else 
     puts "Wow! You are  #{age} years old!"
     puts "#{name}, in #{till100} you will be 100 years old!"
+
